7288a5e803919a45680e6dfc85920801201c15e4
Ingy döt Net <ingy@ingy.net>
2015-06-23 14:41:30 -0700

    Refactoring done.
    Needs testing and try to use new features.

--- lib/pairup.bash
+++ lib/pairup.bash
@@ -1,6 +1,45 @@
 #!/usr/bin/env bash
 
+set -e
+
 export PairUp=.PairUp
+
+RUN() {
+  if [ -z "$cmd" ]; then
+    local cmd="${PAIRUP_ROOT:??}/sbin/${1:?command required}"
+    shift
+  fi
+  [ -z "$title" ] || TITLE "$title"
+  [ -z "$sudo" ] || local sudo='sudo -E'
+  if [ -n "$log" ]; then
+    mkdir -p "$(dirname $log)"
+    $sudo "$cmd" "$@" &>> "$log"
+  else
+    $sudo "$cmd" "$@"
+  fi
+}
+
+TITLE() {
+  (
+    set +x
+    cat <<...
+###############################################################################
+# $1
+###############################################################################
+...
+  )
+}
+
+tests_ok() {
+  local path="$BASH_SOURCE"
+  [[ "$path" =~ ^bin/ ]] && path="./$path"
+  (
+    set +e
+    cd "${path%/bin/*}"
+    make test &> /dev/null
+  ) || return 1
+  return 0
+}
 
 die() { set +x; echo "$@" >&2; exit 1; }
 
@@ -30,51 +69,9 @@ continue?() {
   )
 }
 
-RUN() {
-  local sbin="${PAIRUP_ROOT:??}/sbin"
-  local command="${1:?command required}"; shift
-  [ -z "$title" ] || TITLE "$title"
-  "$sbin/$command" "$@"
-}
-
-TITLE() {
-  (
-    set +x
-    cat <<...
-###############################################################################
-# $1
-###############################################################################
-...
-  )
-}
-
-# SECTION() {
-#   section_command="$1"
-#   TITLE "$1"
-# }
-# 
-# run-section-cmd() {
-#   [ -n "$log" ] ||
-#     die "Missing 'log' running section command '$section_command'"
-#   $section_command &>> $log || carp &>> $log &
-# }
-
-tests_ok() {
-  local path="$BASH_SOURCE"
-  [[ "$path" =~ ^bin/ ]] && path="./$path"
-  (
-    set +e
-    cd "${path%/bin/*}"
-    make test &> /dev/null
-  ) || return 1
-  return 0
-}
-
-export -f carp
-export -f continue?
-export -f die
 export -f RUN
 export -f TITLE
-# export -f SECTION
-# export -f run-section-cmd
 export -f tests_ok
+export -f die
+export -f carp
+export -f continue?
